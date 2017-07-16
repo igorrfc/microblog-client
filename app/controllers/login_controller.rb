76630200e9886044c3c0253243a2457bec6210ee
Igor Fernandes <igorrfc10@gmail.com>
2017-07-16 15:48:34 -0300

    Update the LoginController
    changing the create action to retrieve the authenticated user when authentication succeed.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,7 +5,7 @@ class LoginController < ApplicationController
     if authentication.process
       session[:access_token] = authentication.token
       session[:current_user] = authentication.current_user
-      head :ok
+      render json: { data: authentication.current_user }, status: :ok
     else
       render json: { errors: ['Authentication has failed'] }, status: :bad_request
     end
