b7ab1b0d4cc34df023a1d5781752b0dde540814e
Igor Fernandes <igorrfc10@gmail.com>
2017-07-16 15:46:54 -0300

    Update the container used on the MicroblogApp
    to the AppContainer, which connect all the properties with the other components. Improvements:
      - Reducers management and combination on the store file

--- client/app/bundles/Microblog/startup/registration.jsx
+++ client/app/bundles/Microblog/startup/registration.jsx
@@ -2,7 +2,6 @@ import ReactOnRails from 'react-on-rails';
 
 import MicroblogApp from './MicroblogApp';
 
-// This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   MicroblogApp,
 });
