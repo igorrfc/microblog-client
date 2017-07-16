2be148fba95fa8720e5adc785a23a060b548f790
Igor Fernandes <igorrfc10@gmail.com>
2017-07-16 20:23:06 -0300

    Create the notifications handling actions
    Improvements:
      - Created all the constants which represent the actions;
      - Updated the userReducer to clear visualized notifications;

--- client/app/bundles/Microblog/reducers/userReducer.js
+++ client/app/bundles/Microblog/reducers/userReducer.js
@@ -1,8 +1,11 @@
-import { AUTHENTICATION_SUCCEED } from '../constants';
+import {
+  AUTHENTICATION_SUCCEED,
+  VISUALIZE_NOTIFICATION,
+  REFRESH_USER,
+} from '../constants';
 
 const initialState = {
-  logged: null,
-  list: [''],
+  list: [],
 };
 
 export default function(state = initialState, action) {
@@ -12,6 +15,22 @@ export default function(state = initialState, action) {
         ...state,
         logged: action.user,
       };
+    case REFRESH_USER:
+      return {
+        ...state,
+        logged: action.user,
+      };
+    case VISUALIZE_NOTIFICATION:
+      return {
+        ...state,
+        logged: {
+          ...state.logged,
+          notifications: state.logged.notifications.reduce((acc, cur) => {
+            if (cur.id !== action.notificationId) { acc.push(cur); }
+            return acc;
+          }, []),
+        }
+      }
     default:
       return state;
   }
