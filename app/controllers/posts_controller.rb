f001f70d3b890f007fbdec7d5a4489637a474820
Igor Fernandes <igorrfc10@gmail.com>
2017-07-16 01:17:58 -0300

    Update the PostsController
    retrieving the API's call errors on the returned JSON.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def create
-    post = Post.create(params.merge({access_token: auth.token}))
+    post = Post.create(params.merge({access_token: session[:access_token]}))
 
     if post.errors.blank?
       render json: { data: post }, status: :ok
     else
-      head :unprocessable_entity
+      render json: { errors: post.errors }, status: :unprocessable_entity
     end
   end
 end
