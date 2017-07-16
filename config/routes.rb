f93a8dfba98480457b2a21211f0c4d26de4d96e7
Igor Fernandes <igorrfc10@gmail.com>
2017-07-15 23:52:26 -0300

    Fix route to the login action to point for login#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'microblog_app#index'
 
-  resource :login, controller: 'login', only: :login
+  resource :login, controller: 'login', only: :create
 
   resources :users, only: :index do
     collection do
