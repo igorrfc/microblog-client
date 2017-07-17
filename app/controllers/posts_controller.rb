e2c755d9dad07d22ba1e6726ed613a4722f5ff22
Igor Fernandes <igorrfc10@gmail.com>
2017-07-17 08:23:29 -0300

    Update the PostsController
    changing the way that the Post.create is called. Passing the parameters on the right format.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,22 @@
 class PostsController < ApplicationController
   def create
-    post = Post.create(params.merge({access_token: session[:access_token]}))
+    post = Post.create(
+      title: post_params[:title],
+      description: post_params[:description],
+      access_token: session[:access_token],
+      user_id: params[:user_id],
+    )
 
     if post.errors.blank?
-      render json: { data: post }, status: :ok
+      render json: { data: post.as_json['attributes'] }, status: :ok
     else
       render json: { errors: post.errors }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 end
