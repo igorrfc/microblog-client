e2547bc09e4ab0000b36d4136972c68640838b7a
Igor Fernandes <igorrfc10@gmail.com>
2017-08-20 14:28:55 -0300

    Update the fields' actions module
    changing the handleFieldChange function to run validations before call the callback function and se
    nd the errors when they're present.

--- client/app/bundles/Microblog/actions/fields.js
+++ client/app/bundles/Microblog/actions/fields.js
@@ -1,3 +1,18 @@
-export const handleFieldChange = (callback) => (
-  (event) => callback(event.target.name, event.target.value)
+export const handleFieldChange = (callback, validations = undefined) => (
+  (event) => {
+    let errors = [];
+
+    if (validations) {
+      validations.forEach((validation) => {
+        const processedValidation = validation(event.target.value);
+
+        processedValidation.failed && errors.push(processedValidation.description)
+      })
+    }
+
+    callback(event.target.name, {
+      value: event.target.value,
+      errors: errors,
+    });
+  }
 );
